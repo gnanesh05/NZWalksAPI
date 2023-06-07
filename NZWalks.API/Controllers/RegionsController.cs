0d9b47fbcba9ce05f63cd9ad4156774004cae88e
Gnaneshwer M K <gnanesmk@ezesoft.com>
2023-06-07 17:43:16 +0530

    done with region controller crud functionality

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -16,23 +18,109 @@ namespace NZWalks.API.Controllers
             this.dbContext = dbContext;
         }
         [HttpGet]
-        public IActionResult GetAll() 
+        public async Task<IActionResult> GetAll() 
         { 
-           
-             var regions = dbContext.Regions.ToList() ;
-             return Ok(regions);
+             // data from database - domain models
+             var regions = await dbContext.Regions.ToListAsync() ;
+
+            // data map to dto
+            var regionDTO = new List<RegionDTO>();
+            foreach (var region in regions)
+            {
+                regionDTO.Add(new RegionDTO()
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl,
+                });
+            }
+             return Ok(regionDTO);
 
         }
 
         [HttpGet]
         [Route("{id:Guid}")]
-        public IActionResult GetById([FromRoute] Guid id)
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
         {
-            var region = dbContext.Regions.FirstOrDefault(r => r.Id == id);
+            var region = await dbContext.Regions.FirstOrDefaultAsync(r => r.Id == id);
             if(region == null) { 
                 return NotFound();
             }
-            return Ok(region);
+
+            var regionsDTO = new RegionDTO() 
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                RegionImageUrl = region.RegionImageUrl,
+            };
+           
+            return Ok(regionsDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]RegionCreateDTO region)
+        {
+            var regionDomainModel = new Region()
+            {
+                Code = region.Code,
+                RegionImageUrl = region.RegionImageUrl,
+                Name = region.Name,
+            };
+
+            await dbContext.Regions.AddAsync(regionDomainModel);
+            await dbContext.SaveChangesAsync();
+
+            var regionDTO = new RegionDTO()
+            {
+                Id = regionDomainModel.Id,
+                Name = regionDomainModel.Name,
+                Code = regionDomainModel.Code,
+                RegionImageUrl = regionDomainModel.RegionImageUrl,
+            };
+
+            return CreatedAtAction(nameof(GetById), new {id = regionDTO.Id}, regionDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Create([FromRoute] Guid id, [FromBody] RegionUpdateDTO Updateregion)
+        {
+            var region =  await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            region.Name = Updateregion.Name;
+            region.Code = Updateregion.Code;
+            region.RegionImageUrl = Updateregion.RegionImageUrl;
+            await dbContext.SaveChangesAsync();
+
+            var regionDTO = new RegionDTO()
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                RegionImageUrl = region.RegionImageUrl,
+            };
+            return Ok(regionDTO);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var region = await dbContext.Regions.FirstOrDefaultAsync(y => y.Id == id);
+            if(region == null)
+            {
+                return NotFound();
+            }
+            dbContext.Regions.Remove(region); 
+            await dbContext.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
